21cceb70b682b3b8d7a5b76423d1aca822d79d99
AndreyKoposov <140280539+AndreyKoposov@users.noreply.github.com>
2026-03-19 14:09:50 +0300

    remove unaccessed import

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 from src.app.api.router import router
 
 
